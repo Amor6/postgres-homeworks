5f6760a2ec4fdcadba5aa6104a9c7f68c14c5b92
Amor6 <135590534+Amor6@users.noreply.github.com>
2023-08-03 17:57:49 +0400

    Создание коннектора для ввода данных в БД

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,13 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+connect_db = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password=''
+)
+# Данные для подключение к БД
+
+
