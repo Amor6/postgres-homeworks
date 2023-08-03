7d7dad1a5d66b1302a80f93328f7d3205727e891
Amor6 <135590534+Amor6@users.noreply.github.com>
2023-08-03 17:59:45 +0400

    создание блока для заполнения данных employees

--- homework-1/main.py
+++ homework-1/main.py
@@ -10,4 +10,16 @@ connect_db = psycopg2.connect(
 )
 # Данные для подключение к БД
 
+try:
+    with connect_db:
+        with connect_db.cursor() as cursor_db:
+            with open('north_data/employees_data.csv', encoding='UTF-8') as f:
+                file_dict = csv.DictReader(f, delimiter=',')
+                for id, line in enumerate(file_dict, 1):
+                    cursor_db.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                                      (id, line['first_name'], line['last_name'], line['title'], line['birth_date'],
+                                       line['notes']))
+        cursor_db.close()
+        # Заполнение таблицы employees
+
 
